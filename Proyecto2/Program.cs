03b4f086649f8b5c4b8b216b2026ab1521845c21
CervantesSanti2005 <SantiagoCervantes@ciencias.unam.mx>
2024-10-04 03:39:50 -0600

    Punto de entrada real

--- Proyecto2/Program.cs
+++ Proyecto2/Program.cs
@@ -2,9 +2,27 @@ using System;
 
 namespace BDMusica{
     static class Program{
-        static void Main(){
-                Console.WriteLine("hola mundo!");
+        static void Main(string[] args){
+                // Inicializar la base de datos (se creará en el archivo 'music.db')
+            string dbPath = "music.db";
 
+            var bd = new BaseDeDatos();
+            bd.Database(dbPath);
+
+            // Crear las tablas y asegurarse de que están listas
+            bd.crearTabla();
+            bd.insertarDefault();
+
+            // Inicializar el minero de MP3 y comenzar a minar el directorio
+            var minero = new Minero(bd);
+
+
+            string directoryPath = @"Downloads/";  // Cambia esto a la ruta de los archivos MP3
+            Console.WriteLine("Minando archivos en: " + directoryPath);
+
+            minero.minarDirectorio(directoryPath);
+
+            Console.WriteLine("Listo");
         }
     }
 }
--- Proyecto2/obj/Debug/net8.0/Proyecto2.AssemblyInfo.cs
+++ Proyecto2/obj/Debug/net8.0/Proyecto2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+548706a138ff075b6ea519759093e805f077312b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4321d409136fd9ba3f67c9fc99f3912c36729678")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
