b18ca1ba08b2b584121bc71f30c736013199d1a5
CervantesSanti2005 <SantiagoCervantes@ciencias.unam.mx>
2024-10-10 23:01:40 -0600

    Arreglos a la interfaz

--- Proyecto2/obj/Debug/net8.0/Proyecto2.AssemblyInfo.cs
+++ Proyecto2/obj/Debug/net8.0/Proyecto2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc3dc1459cc71d4ae1c51d053c0b7aa98923e9c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d139dcd4532b8b4bdd8125939b282577775c2a6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
